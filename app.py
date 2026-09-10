f0f9c9a4a00b517b906c9a141578cacbfe63c96a
Pinkymdlc <rosa.malmcrona@gmail.com>
2026-09-10 16:32:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,13 @@ if st.button("Procesar"):
         modelo = partes[0].strip()
         url = partes[1].strip()
 
-        datos.append({
-            "MODELO": modelo,
-            "URL": url
-        })
+datos.append({
+    "MODELO": modelo,
+    "URL": url,
+    "POSICION": "",
+    "SELLER": "",
+    "CARREFOUR": ""
+})
 
     df = pd.DataFrame(datos)
 
