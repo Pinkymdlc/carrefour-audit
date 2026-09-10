5c25384e8d2ed84700935aa2a080714b23a1151b
Pinkymdlc <rosa.malmcrona@gmail.com>
2026-09-10 16:00:01 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+ 
+st.title("Auditor Carrefour")
+ 
+archivo = st.file_uploader(
+"Sube el Excel",
+type=["xlsx"]
+)
+ 
+if archivo:
+ 
+df = pd.read_excel(archivo)
+ 
+st.write("Datos cargados")
+ 
+st.dataframe(df)
+ 
+if st.button("Procesar"):
+ 
+df["POSICION"] = ""
+df["SELLER"] = ""
+df["CARREFOUR"] = ""
+ 
+st.dataframe(df)
